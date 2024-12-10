12c0e9c7a773f75b87efae89478612469341011b
alespo10 <alesposato15@gmail.com>
2024-12-10 11:06:57 +0100

    Modifica del controller su come calcolare soluzioni.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,12 +1,8 @@
 package Controller;
 
-
-import Model.Blocco;
-import Model.Casella;
 import Model.Griglia;
 import Model.Soluzione;
 import View.Grafica;
-
 import javax.swing.*;
 import java.util.List;
 
@@ -14,6 +10,7 @@ import java.util.List;
 public class Controller {
     private final Grafica grafica;
     private Griglia griglia;
+    private List<int[][]> soluzioni;
 
     public Controller() {
         this.grafica = new Grafica();
@@ -29,34 +26,24 @@ public class Controller {
                 griglia = new Griglia(dimensioneMatrice);
                 griglia.popolaGriglia();
 
+                Soluzione solver = new Soluzione(griglia, grafica.getNumeroSoluzioni());
+                solver.risolvi();
+                soluzioni = solver.getSoluzioni();
+                System.out.println(soluzioni);
+
                 grafica.mostraSchermata("gioco");
                 grafica.mostraGriglia(griglia.getGriglia(), griglia.getBlocchi());
             }
         });
 
-        //Listner di verifica della soluzioen finale
+        //Listner di verifica della soluzione finale
         grafica.setVerificaListener(e -> {
-            Griglia grigliaVuota = creaGrigliaVuota(griglia);
-            System.out.println("Struttura della Griglia Vuota:");
-            for (Blocco blocco : grigliaVuota.getBlocchi()) {
-                System.out.println("Vincolo=" + blocco.getTipoVincolo() + ", Risultato=" + blocco.getRisultatoVincolo());
-                for (Casella casella : blocco.getCaselle()) {
-                    System.out.println("  Cella: (" + casella.getX() + ", " + casella.getY() + "), Valore=" + casella.getValore());
-                }
-            }
-            // Ottieni i valori attuali inseriti dall'utente
             int[][] valoriInseriti = griglia.getValoriAttuali();
             System.out.println("Valori Inseriti:");
             for (int[] row : valoriInseriti) {
                 System.out.println(java.util.Arrays.toString(row));
             }
 
-            //Richiamo la classe Soluzione passando la griglia vuota su cui generare le soluzioni
-            // Se passavo quella originale con i numeri inseriti causa errori
-            Soluzione solver = new Soluzione(grigliaVuota, grafica.getNumeroSoluzioni());
-            solver.risolvi();
-
-            List<int[][]> soluzioni = solver.getSoluzioni();
             System.out.println("Soluzioni Generate:");
             for (int[][] soluzione : soluzioni) {
                 for (int[] row : soluzione) {
@@ -87,27 +74,6 @@ public class Controller {
         // aggiungere metodo che cambia gli stati dei vari pulsanti
     }
 
-    private Griglia creaGrigliaVuota(Griglia griglia) {
-        Griglia grigliaVuota = new Griglia(griglia.getDimensione());
-        for (Blocco bloccoOriginale : griglia.getBlocchi()) {
-            Blocco bloccoVuoto = new Blocco(
-                    bloccoOriginale.getTipoVincolo(),
-                    bloccoOriginale.getRisultatoVincolo()
-            );
-            // Itera sulle caselle del blocco originale
-            for (Casella casellaOriginale : bloccoOriginale.getCaselle()) {
-                Casella casellaVuota = grigliaVuota.getCella(
-                        casellaOriginale.getX(),
-                        casellaOriginale.getY()
-                );
-                casellaVuota.setBlocco(bloccoVuoto);
-                bloccoVuoto.aggiungiCasella(casellaVuota);
-                casellaVuota.setValore(0);
-            }
-            grigliaVuota.aggiungiBlocco(bloccoVuoto);
-        }
-        return grigliaVuota;
-    }
 
     private boolean confrontaSoluzione(int[][] valoriUtente, int[][] soluzione) {
         int dimensione = valoriUtente.length;
